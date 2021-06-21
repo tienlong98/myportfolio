217020f97953493960fd5acb6440a2780e05ec7d
PHAM THANH LONG <c210117@ccg.ac.jp>
2021-06-22 01:11:17 +0900

    update menu

--- js/script.js
+++ js/script.js
@@ -1,6 +1,31 @@
-var typed = new Typed(".typing", {
-    strings: ["ウェブサイト開発者", "ウェブサイトデザイン"],
-    typeSpeed: 200,
-    backSpeed: 120,
-    loop: true
+$(document).ready(function() {
+    $(window).scroll(function() {
+        // sticky navbar on scroll script
+        //if (this.scrollY > 20) {
+        //    $('.main_menu').addClass("sticky");
+        // } else {
+        //      $('.main_menu').removeClass("sticky");
+        // }
+
+        // scroll-up button show/hide script
+        if (this.scrollY > 400) {
+            $('.scroll-up-btn').addClass("show");
+        } else {
+            $('.scroll-up-btn').removeClass("show");
+        }
+    });
+    $('.scroll-up-btn').click(function() {
+        $('html').animate({ scrollTop: 0 });
+        // removing smooth scroll on slide-up button click
+        $('html').css("scrollBehavior", "auto");
+    });
+
+
+
+    var typed = new Typed(".typing", {
+        strings: ["ウェブサイト開発者", "ウェブサイトデザイン"],
+        typeSpeed: 200,
+        backSpeed: 120,
+        loop: true
+    });
 });
